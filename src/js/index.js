77a31cdbe52f0238c1e1abcb0d5ed8b6c6e8fcf6
ReginaEllen <koenig.regina1970@gmail.com>
2024-02-03 21:11:56 +0100

    add function to playbutton

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,15 @@ const result = multiply(2, 5);
 
 console.log(result);
 
+var playButton = document.getElementById("play");
 
+// Funktion hinzufügen, die beim Klicken auf das play-Element aufgerufen wird
+playButton.addEventListener("click", function() {
+    // Das Video-Element abrufen
+    var video = document.getElementById("video").querySelector("video");
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+});
