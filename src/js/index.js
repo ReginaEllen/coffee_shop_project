874d8766954baf4f3be6f83bf4f87dbee50a085e
ReginaEllen <koenig.regina1970@gmail.com>
2024-02-05 21:28:49 +0100

    add javascript to burger-menu and close-button

--- src/js/index.js
+++ src/js/index.js
@@ -6,12 +6,21 @@ const result = multiply(2, 5);
 
 console.log(result);
 
+// mein Code //
+
 
 var burgermenu = document.getElementById("open"); 
 
 burgermenu.addEventListener("click", function() {
-    var overlay = document.getElementById("overlay");
+    var overlay = document.getElementById("overlay");     
     overlay.style.display = "block"
+});
+
+var close_button = document.getElementById("close-button");
+
+close_button.addEventListener("click", function() {
+    var overlay = document.getElementById("overlay"); 
+    overlay.style.display = "none"
 });
 
 
