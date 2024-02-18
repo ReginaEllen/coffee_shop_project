1d4ea0fee4946a106306f26e11e500489b868fe4
ReginaEllen <koenig.regina1970@gmail.com>
2024-02-18 18:49:20 +0100

    fix bugs dropdown-funcionality

--- src/js/select.js
+++ src/js/select.js
@@ -3,15 +3,44 @@ document.addEventListener("DOMContentLoaded", function() {
     var customArrowDown = document.getElementById("custom-arrow-down");
     var customArrowUp = document.getElementById("custom-arrow-up");
 
+    var inputs = document.querySelectorAll("input");
+    var textarea =  document.querySelector("textarea");
+
+    function updateArrow() {
+        if (dropdown.getAttribute("data-open") === "true") {
+            customArrowDown.style.display = "none";
+            customArrowUp.style.display = "block";
+        } else {
+            customArrowDown.style.display = "block";
+            customArrowUp.style.display = "none";
+        }
+    }
+
     dropdown.addEventListener("click", function() {
-        if (dropdown.getAttribute("data-close") === "true") {
-            dropdown.setAttribute("data-close", "false");
+        if (dropdown.getAttribute("data-open") === "true") {
+            dropdown.setAttribute("data-open", "false");
             customArrowDown.style.display = "block";
             customArrowUp.style.display = "none";
         } else {
-            dropdown.setAttribute("data-close", "true");
+            dropdown.setAttribute("data-open", "true");
             customArrowDown.style.display = "none";
             customArrowUp.style.display = "block";
         }
     });
+
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener("click", function() {
+            customArrowDown.style.display = "block";
+            customArrowUp.style.display = "none";
+            dropdown.setAttribute("data-open", "false")
+        });
+    }
+
+    textarea.addEventListener("click", function() {
+        customArrowDown.style.display = "block";
+        customArrowUp.style.display = "none";
+        dropdown.setAttribute("data-open", "false")
+    });
+
+    updateArrow();
 });
