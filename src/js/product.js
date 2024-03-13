9b6770e5803e6fd57bcf003e20b55c33ba29b777
ReginaEllen <koenig.regina1970@gmail.com>
2024-03-13 19:42:28 +0100

    add product page

--- src/js/product.js
+++ src/js/product.js
@@ -11,17 +11,62 @@ const product = async () => {
   );
 
   const productHtml = `
-      <div class=coffee>
-        <div class="product">
+  
+      <div class="coffee-details">
+        <div class="product-details">
           <img src="${productImgPath}" alt="product image">
         </div>
-        <p>${product.productName}</p>
-      
-        <p class="price">${product.price / 100}€</p>
-        <div class="product-description">
-          ${product.description}
+
+        <div class="information">
+          <h3>${product.productName}</h3>
+          <p class="price">${product.price/100}€</p>
+          <p class="product-features">${product.features}</p>
         </div>
-      </div>
+
+        <div id="icons">
+
+          <div>
+            <img id="machine" src="../images/coffee_machine.svg" alt="Icon Kaffeemaschine">
+            <p>zum Filtern gemahlen</p>
+          </div>
+
+          <div>
+            <img id="beans" src="../images/Icon Kaffee Bohnen.svg" alt="Icon Kaffeebohnen">
+            <p>ganze Bohnen</p>
+          </div>
+
+          <div>
+            <img id="press" src="../images/Icon French Press.svg" alt="Icon French Press">
+            <p>für French Press</p>
+          </div>
+        </div>
+
+        <div class="select-container">
+
+          <form>
+            <select class="select-box" id="dropdown">
+                <option id="select_label" value="">Wieviel Kaffee brauchst du?</option>
+                <option value="delivery">250g</option>
+                <option value="product">500g</option>
+                <option value="sale">1000g</option>
+            </select>
+
+            <img class="custom-arrow" id="custom-arrow-up" src="../images/Button hell_1.png" alt="dropdown-button">
+            <img class="custom-arrow" id="custom-arrow-down" src="../images/Button hell.png" alt="dropdown-button">
+
+            <div>
+              <input class="button-dark" type="submit" value="In den Warenkorb">
+            </div>
+
+          </form>
+
+        </div>
+
+        <div class="product-description">
+          <h3>Beschreibung</h3>
+          <p>${product.description}</p>
+        </div>
+    </div>
   `;
 
   const productContainer = document.querySelector(".product-container");
